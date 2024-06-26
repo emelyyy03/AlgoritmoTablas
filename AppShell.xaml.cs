1fc2bffe90a9f27457bb2e8d6faed4c99a157354
Indel <Indel@DESKTOP-BEC38OO>
2024-06-26 14:15:46 -0600

    Agregar archivos de proyecto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgoritmoTablas
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace AlgoritmoTablas
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ScrollView scrollView = new ScrollView();
+            Content = scrollView;
+
+            StackLayout stackLayout = new StackLayout();
+            scrollView.Content = stackLayout;
+
+            for (int numero=1; numero <= 10; numero++)
+            {
+                Label TituloTabla = new Label();
+                TituloTabla.Text = $"Tabla Del {numero}";
+                stackLayout.Children.Add(TituloTabla);
+
+                for(int multiplicador = 1; multiplicador <= 10; multiplicador++)
+                {
+                    int resultado = numero * multiplicador;
+                    Label lineaTabla = new Label();
+                    lineaTabla.Text=$"{numero}x{multiplicador}={resultado}";
+                    stackLayout.Children.Add(lineaTabla);
+                }
+
+                stackLayout.Children.Add(new Label { Text = "" });
+            }
+
+
+        }
+
+    }
+
+}
